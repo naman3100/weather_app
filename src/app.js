5d9b3021f668547bcf5c476df9c413240eed2429
Naman Agarwal <naman.mitian@gmail.com>
2019-05-14 15:33:07 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const hbs=require("hbs")
 const geocode=require("./utils/geocode")
 const forecast=require("./utils/forecast")
 
+//Port no
+const port=process.env.PORT || 3000;
+
 //Define Paths for ecpress config
 const publicDirectoryPath =  path.join(__dirname,'../public')
 const viewPath=path.join(__dirname,"../templates/views")
@@ -79,6 +82,6 @@ app.get("*",(req,res)=>{
     })
 })
 
-app.listen(3000, ()=>{
-    console.log('Server is up in port 3000')
+app.listen(port, ()=>{
+    console.log('Server is up on port ' + port)
 })
